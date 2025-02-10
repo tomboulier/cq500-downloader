2fe532b82d8ce4997d61796cd02b44de7c040da4
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-02-10 14:53:01 +0100

    refactor: extract function for downloading a single patient

--- src/cq500downloader/domain/usecases/download.py
+++ src/cq500downloader/domain/usecases/download.py
@@ -6,6 +6,30 @@ from urllib.request import urlretrieve
 logger = logging.getLogger(__name__)
 
 
+def download_patient(patient_number: int, destination_file: Path) -> None:
+    """
+    Downloads a single patient from the CQ500 dataset.
+
+    Parameters
+    ----------
+    patient_number : int
+        Patient number to download
+    destination_file : Path, optional
+        Directory where the dataset will be downloaded, by default "cq500"
+    """
+    url_base = "https://s3.ap-south-1.amazonaws.com/qure.headct.study"
+    patient_base_name = f"CQ500-CT-{patient_number}"
+    url_patient = f"{url_base}/{patient_base_name}.zip"
+
+    try:
+        logger.info(f"Downloading {url_patient}...")
+        urlretrieve(url=url_patient, filename=destination_file)
+        logger.info(f"File downloaded to {destination_file}")
+    except Exception as error:
+        logger.error(f"Error downloading {url_patient}: {error}")
+        raise error
+
+
 def download_dataset(destination: Path = Path("cq500")) -> None:
     """
     Downloads the CQ500 dataset using wget or aria2.
@@ -20,12 +44,5 @@ def download_dataset(destination: Path = Path("cq500")) -> None:
 
     for patient_number in range(491):
         base_name = f"CQ500-CT-{patient_number}"
-        try:
-            url_patient = f"{url_base}/CQ500-CT-{patient_number}.zip"
-            zip_filename = f"{destination}/{base_name}.zip"
-            logger.info(f"Downloading {url_patient}...")
-            urlretrieve(url=url_patient, filename=zip_filename)
-            logger.info(f"File downloaded to {zip_filename}")
-        except Exception as error:
-            logger.error(f"Error downloading {url_patient}: {error}")
-            raise error
+        download_patient(patient_number, Path(f"{destination}/{base_name}.zip"))
+
