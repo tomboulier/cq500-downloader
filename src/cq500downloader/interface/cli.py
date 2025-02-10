24ce2d9dfdeeb4c2eeef44218c0bc37cf200182c
Thomas Boulier <14161960+tomboulier@users.noreply.github.com>
2025-02-10 14:12:25 +0100

    refactor: move cli function to interface layer

---
+++ src/cq500downloader/interface/cli.py
@@ -0,0 +1,18 @@
+import argparse
+
+from src.cq500downloader.domain.usecases.download import download_dataset
+
+
+def cli() -> None:
+    """
+    Command-line interface for the CQ500 dataset downloader.
+    """
+    parser = argparse.ArgumentParser(description="CQ500 Dataset Downloader and Processor")
+    parser.add_argument("--download", action="store_true", help="Download the CQ500 dataset")
+    parser.add_argument("--destination-folder", type=str, default="cq500", help="Destination folder for the dataset")
+    args = parser.parse_args()
+
+    if args.download:
+        download_dataset(destination=args.destination_folder)
+    else:
+        parser.print_help()
